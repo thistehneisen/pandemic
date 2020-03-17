f669b2206007c1cb197458a72d814813b0a182c9
Nils Putnins <putnins@nils.digital>
2020-03-17 18:38:46 +0200

    Change zoom level to 9

--- assets/scripts/pandemic.js
+++ assets/scripts/pandemic.js
@@ -115,7 +115,7 @@ $(document).ready(function(){
         div: '#map',
         lat: 56.946618,
         lng: 24.097274,
-        zoom: 8,
+        zoom: 9,
         styles: [
             {
                 "featureType": "all",
