bb2ce6b30e7a91eef57a8c64043f4e4d3c6e1473
Nils Putnins <putnins@nils.digital>
2020-03-17 22:32:58 +0200

    Modifying json_data.php

--- json_data.php
+++ json_data.php
@@ -1,14 +1,18 @@
 <?php
 require_once './vendor/autoload.php';
-
 use \League\Csv\Reader;
 
-if (!ini_get("auto_detect_line_endings")) {
-    ini_set("auto_detect_line_endings", '1');
+if (!isset($_GET['action']) && empty($_GET['action'])) {
+    if (!ini_get("auto_detect_line_endings")) {
+        ini_set("auto_detect_line_endings", '1');
+    }
+
+    $csv = Reader::createFromPath(dirname(__FILE__) . '/data.csv', 'r');
+    $csv->setHeaderOffset(0);
+
+    header('Content-Type: application/json');
+    echo json_encode($csv, JSON_PRETTY_PRINT);
+} else if ($_GET['action'] == 'stats') {
+    $contents = file_get_contents('https://pomber.github.io/covid19/timeseries.json');
+    print($contents);
 }
-
-$csv = Reader::createFromPath(dirname(__FILE__) . '/data.csv', 'r');
-$csv->setHeaderOffset(0);
-
-header('Content-Type: application/json');
-echo json_encode($csv, JSON_PRETTY_PRINT);
