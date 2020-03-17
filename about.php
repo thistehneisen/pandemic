d0115dbe536b90d208d1a16d8ef95079c795c363
Nils Putnins <putnins@nils.digital>
2020-03-17 22:42:41 +0200

    Updating About section

--- about.php
+++ about.php
@@ -1,2 +1,12 @@
-<h2>About project</h2>
-<p>We want you to have no more useless information.</p>
+<h2>About PANDEMIC.LV</h2>
+<p>In the time of crisis, information overflow is inevitable. We're fixing the problem by the possibility to access, and share information — based on regions.</p>
+<?php
+$contents = file_get_contents('https://pomber.github.io/covid19/timeseries.json');
+$contents = json_decode($contents, true);
+$contents = $contents['Latvia'];
+?>
+<p><strong>Statistics:</strong><br/>
+<ol>
+<?php foreach ($contents as $entry) { print('<li>Date: ' . $entry['date'] . '<br/>Confirmed: ' . $entry['confirmed'] . '<br/>Deaths: ' . $entry['deaths'] . '<br/>Recovered: ' . $entry['recovered'] . '</li>'); } ?>
+</ol>
+</p>
