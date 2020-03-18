08a00424312eb8920bb4106d4d0f4b194709a24d
Martin Turoci <martinturoci@gmail.com>
2020-03-18 19:29:21 +0100

    feat: graph statistics

--- about.php
+++ about.php
@@ -6,6 +6,86 @@ $contents = json_decode($contents, true);
 $contents = array_reverse($contents['Latvia']);
 ?>
 <strong>Statistics:</strong><br/>
+
+<canvas id="mainChart"></canvas>
+
 <ol>
 <?php foreach ($contents as $entry) { print('<li><strong>' . $entry['date'] . '</strong>, ' . $entry['confirmed'] . ' confirmed with ' . $entry['deaths'] . ' deaths and ' . $entry['recovered'] . ' recoveries</li>'); } ?>
 </ol>
+
+<script>
+  const ctx = document.getElementById("mainChart").getContext("2d");
+  fetch("https://pomber.github.io/covid19/timeseries.json").then(async res => {
+    const json = await res.json();
+    const latestData = json.Latvia.slice(-15);
+    const labels = latestData.map(entry => entry.date);
+    const colors = {
+      green: {
+        fill: "#e0eadf",
+        stroke: "#5eb84d"
+      },
+      lightBlue: {
+        stroke: "#6fccdd"
+      },
+      darkBlue: {
+        fill: "#92bed2",
+        stroke: "#3282bf"
+      },
+      purple: {
+        fill: "#8fa8c8",
+        stroke: "#75539e"
+      }
+    };
+
+    const recovered = latestData.map(entry => entry.recovered);
+    const deaths = latestData.map(entry => entry.deaths);
+    const confirmed = latestData.map(entry => entry.confirmed);
+
+    const myChart = new Chart(ctx, {
+      type: "line",
+      data: {
+        labels,
+        datasets: [
+          {
+            label: "Recovered",
+            fill: true,
+            backgroundColor: colors.green.fill,
+            pointBackgroundColor: colors.green.stroke,
+            borderColor: colors.green.stroke,
+            pointHighlightStroke: colors.green.stroke,
+            data: recovered
+          },
+          {
+            label: "Confirmed",
+            fill: true,
+            backgroundColor: colors.darkBlue.fill,
+            pointBackgroundColor: colors.darkBlue.stroke,
+            borderColor: colors.darkBlue.stroke,
+            pointHighlightStroke: colors.darkBlue.stroke,
+            borderCapStyle: "butt",
+            data: confirmed
+          },
+          {
+            label: "Deaths",
+            fill: true,
+            backgroundColor: colors.green.fill,
+            pointBackgroundColor: colors.lightBlue.stroke,
+            borderColor: colors.lightBlue.stroke,
+            pointHighlightStroke: colors.lightBlue.stroke,
+            borderCapStyle: "butt",
+            data: deaths
+          }
+        ]
+      },
+      options: {
+        scales: {
+          yAxes: [
+            {
+              stacked: true
+            }
+          ]
+        }
+      }
+    });
+  });
+</script>
