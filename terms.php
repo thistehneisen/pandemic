e7c091d3285135df4e13bc3adba4668a8dd53b22
Nils Putnins <putnins@nils.digital>
2020-03-21 01:48:15 +0200

    Terms of Usage / Privacy Policy 1.0

--- terms.php
+++ terms.php
@@ -1,4 +1,18 @@
-	<h2>Lietošanas noteikumi</h2>
-	<p>Pievienojot failus, tu apliecini ka izmantotie attēli pieder tev, kā arī visu norādīto informāciju atļauj mums izmantot portālā.</p>
-	<p>Pakalpojums tiek sniegts "<em>kāds tas ir</em>" variantā, tādēļ nenesam nekādu atbildību par iespējamiem zaudētiem datiem, kļūmēm, vai neērtībām tā lietošanas rezultātā.</p>
-	<p>Dati atrodas uz <a href="http://influent.solutions/">"INFLUENT" SIA</a> un <a href="http://seq.science/">"Digital Security Alliance" biedrības</a> serveriem.</p>
+	<h2>Terms of Service <em>&</em> Privacy Policy</h2>
+	<strong>Terms used:</strong>
+	<ul>
+		<li>"<em>Platform</em>" — The site that is either <a href="https://pandemic.lv">pandemic.lv</a>, <a href="https://pandemic.lt">pandemic.lt</a> or <a href="https://pandemic.ee">pandemic.ee</a>.
+	</ul>
+
+	<p>By adding the any content to the <em>Platform</em>, you confirm that you have the right to the content. By creating the content, comfirm that <em>Platform</em> has full permission to use and store the content.</p>
+	<p>The user himself takes full responsibility of the content that he/she has shared to the <em>Platform</em>, we have the right to remove any content based on our subjective views, at any time, without any prior warning.</p>
+	<p>This <em>Platform</em> is provided "<em>as is</em>", we take no responsibility for possible loss of data, errors, and any other problems that can occur whilst using the <em>Platform</em>.</p>
+	<p>It is <strong>strictly</strong> prohibited to engage in threats of bodily or physical harm or any kind of harassment using any method including by email, member to member messaging, our chat system or any other source that's given by the <em>Platform</em>. If a member receives a threat, it should be reported to local law enforcement</p>
+	<p>Hateful, obscene, offensive, profane, racist, sexual, defamatory, or violent language cannot be used at any area of the <em>Platform</em></p>
+	<p>You are accepting that in order to function, <em>Platform</em> <strong>is permitted</strong> to create/use/modify browsers "<em>Cookies</em>" and store data such as:</p>
+	<ul>
+		<li>Upon authorization with <strong>Facebook</strong>: your unique APP USER ID, Profile Picture, Full Name and IP address.</li>
+		<li>Upon authorization with <strong>Google Maps</strong> (<em>GeoLocation</em>): your current latitude, longitude and time.</li>
+		<li>We reserve the right to store extra data for the purposes of statistics, debugging and improving our <em>Platform</em></li>
+	</ul>
+	<p>Our platform has been secured and audited by <a href="https://seq.science/" target="_blank">Digital Security Alliance</a>. In case of any inquiries, contact us at <a href="mailto:info@pandemic.lv?subject=ToS inquiry">info@pandemic.lv</a></p>
