75e9cc93b5860400ec608eb0c336e55f3e741bef
Nils Putnins <putnins@nils.digital>
2020-03-20 19:09:44 +0200

    Pandemic icon = red

--- assets/scripts/pandemic.js
+++ assets/scripts/pandemic.js
@@ -322,10 +322,10 @@ $(document).ready(function(){
 
     var quarantineIcon = {
         path: 'M12 2C8.13 2 5 5.13 5 9c0 5.25 7 13 7 13s7-7.75 7-13c0-3.87-3.13-7-7-7zm0 9.5c-1.38 0-2.5-1.12-2.5-2.5s1.12-2.5 2.5-2.5 2.5 1.12 2.5 2.5-1.12 2.5-2.5 2.5z',
-        fillColor: '#222',
+        fillColor: '#ff0000',
         fillOpacity: 0.95,
         scale: 1.5,
-        strokeColor: '#0199d2',
+        strokeColor: '#9f0000',
         strokeWeight: 2,
         anchor: new google.maps.Point(12, 24)
     };
