f7872bee53bd74557ff25c32fb25cf58d9693189
Nils Putnins <putnins@nils.digital>
2020-03-20 19:20:44 +0200

    Modify ajax.php

--- library/ajax.php
+++ library/ajax.php
@@ -53,7 +53,6 @@ if (!empty($_POST)) {
             $userdata = $db->getRow("SELECT * FROM %s WHERE `id`='%d'", $db->users, $location['fbid']);
 
             $nameDetails = explode(" ", trim($userdata['name']));
-
             $namePut = $nameDetails[0].' '.substr($nameDetails[1], 0, 1).'.';
 
             $output[] = [
