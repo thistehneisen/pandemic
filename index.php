4d6c6acc6bc9dffddcb30882ba7cd300f2065095
Nils Putnins <putnins@nils.digital>
2020-03-21 02:56:28 +0200

    Spaces and tabs

--- index.php
+++ index.php
@@ -63,15 +63,13 @@ Write us on info@<?php print($settings['host'])?> and become one of our team.
 	<div id="preloader">
 		<h1>Pandemic Baltics (<em><?php print($settings['country'])?></em>)</h1>
 		<div class="cube-wrapper">
-			
-		<div class="cube-folding">
-			<span class="leaf1"></span>
-			<span class="leaf2"></span>
-			<span class="leaf3"></span>
-			<span class="leaf4"></span>
-		</div>
-
-		<span class="loading" data-name="Loading">Hold on, fetching the data…</span>
+			<div class="cube-folding">
+				<span class="leaf1"></span>
+				<span class="leaf2"></span>
+				<span class="leaf3"></span>
+				<span class="leaf4"></span>
+			</div>
+			<span class="loading" data-name="Loading">Hold on, fetching the data…</span>
 		</div>
 
 		<div class="made-with-love">
