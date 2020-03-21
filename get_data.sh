b6655e73e30deec96f91f0c11a5a66813e0ab222
Nils Putnins <putnins@nils.digital>
2020-03-21 21:44:04 +0200

    Adding another source

--- get_data.sh
+++ get_data.sh
@@ -2,3 +2,6 @@
 
 wget 'https://docs.google.com/spreadsheets/d/e/2PACX-1vQ6e2eYL_dsCeuT6IFKcw07PD1ZY_kjhGtHDGlzPvKhID7_3nTgkQsbLux4J3jaLPomOUeWtW7kxe4c/pub?gid=0&single=true&output=csv' \
     -O data.csv
+
+wget 'https://raw.githubusercontent.com/datasets/covid-19/master/time-series-19-covid-combined.csv' \
+    -O combined.csv
