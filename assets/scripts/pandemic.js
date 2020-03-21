4b50c9ceee1fd3ddafbe2dd16545ac42f59ea559
Nils Putnins <putnins@nils.digital>
2020-03-21 16:11:59 +0200

    Mapbox yolo

--- assets/scripts/pandemic.js
+++ assets/scripts/pandemic.js
@@ -398,16 +398,7 @@ function geoLocate() {
 }
 
 function getIcon(fC = '00aeef', sColor = 222, sC = 1.2, fO = 0.65, url = undefined) {
-    return {
-        path            : 'M12 2C8.13 2 5 5.13 5 9c0 5.25 7 13 7 13s7-7.75 7-13c0-3.87-3.13-7-7-7zm0 9.5c-1.38 0-2.5-1.12-2.5-2.5s1.12-2.5 2.5-2.5 2.5 1.12 2.5 2.5-1.12 2.5-2.5 2.5z',
-        fillColor       : '#'+fC,
-        fillOpacity     : fO,
-        scale           : sC,
-        strokeColor     : '#'+sC,
-        strokeWeight    : 2,
-        url             : url,
-        anchor          : new google.maps.Point(12, 12)
-    };
+    return true;
 }
 
 function dismissPreloader() {
