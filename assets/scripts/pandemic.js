dcf1ba26fb71aadeb6d61904b65aed65f3fa562f
Nils Putnins <putnins@nils.digital>
2020-03-21 12:01:42 +0200

    Debug

--- assets/scripts/pandemic.js
+++ assets/scripts/pandemic.js
@@ -34,8 +34,11 @@ function pandemicSettings(action, sub, data) {
 }
 
 function req(postData) {
+    console.log(postData);
     var a = postData.a,
         m = postData.m;
+    console.log(a);
+    console.log(m);
 
     var response = $.ajax({
         url         : xhr,
@@ -61,7 +64,7 @@ function req(postData) {
         fail        : function (reason, xhr) { if (pandemic.debug === true) { toastr.error(reason + ' XHR: ' + xhr, a + ': ' + m);  } }
     }).responseJSON;
 
-    console.log(response);
+    console.log('Response: ' + response);
 }
 
 function pandemicData(action, sub, data) {
