e0bbcd0645fa041f101930c4f7cc32ccbec911d3
Nils Putnins <putnins@nils.digital>
2020-03-21 04:51:00 +0200

    Retrying AJAX once again

--- assets/scripts/pandemic.js
+++ assets/scripts/pandemic.js
@@ -459,7 +459,7 @@ $(document).ready(function() {
                         toastr.success('Welcome back, ' + response.name + '! Redirecting in a moment…', 'Logged in');
                         fbId = response.id;
                         $.get('', function(response){
-                            $('body header').children().remove().append($(response).find('body header').removeClass('preload-hide').children().fadeIn().html());
+                            $('body header').html($(response).find('body header').removeClass('preload-hide').children().fadeIn().append());
                         });
                         //setTimeout(function(){ window.location.reload(); }, 500);
                     });
