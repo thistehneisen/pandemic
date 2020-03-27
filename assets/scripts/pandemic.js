76ebbc5793015e03d9761236842c8e48c56efb93
Nils Putnins <putnins@nils.digital>
2020-03-27 20:04:24 +0200

    Chat implementing

--- assets/scripts/pandemic.js
+++ assets/scripts/pandemic.js
@@ -335,6 +335,7 @@ $(document).ready(function() {
                 return false;
             } else if (elem.val().length > 0) {
                 var r = pandemicData('chat', 'send', {t:elem.data('t'),m:elem.val()});
+                console.log(r);
                 if (typeof r.success !== 'undefined') {
                     alert('Seeent!');
                 }
